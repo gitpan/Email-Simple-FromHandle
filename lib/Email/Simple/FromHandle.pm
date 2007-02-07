28dcb02f6c25ad837e203510b0414fc1233a012e
Ricardo SIGNES <rjbs@cpan.org>
2007-02-07 19:54:15 +0000

    Import of RJBS/Email-Simple-FromHandle-0.050 from CPAN.
    gitpan-cpan-distribution: Email-Simple-FromHandle
    gitpan-cpan-version:      0.050
    gitpan-cpan-path:         RJBS/Email-Simple-FromHandle-0.050.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/Simple/FromHandle.pm
+++ lib/Email/Simple/FromHandle.pm
@@ -1,60 +1,208 @@
-package Email::Simple::FromHandle;
-use base qw(Email::Simple);
-
 use strict;
 
-use Carp;
+package Email::Simple::FromHandle;
+use base qw(Email::Simple);
+## no critic RequireUseWarnings
+
+=head1 NAME
+
+Email::Simple::FromHandle - an Email::Simple but from a handle
+
+=head1 VERSION
+
+  $Id: /my/cs/projects/fromhandle/trunk/lib/Email/Simple/FromHandle.pm 22478 2006-06-13T12:51:46.331840Z rjbs  $
+
+version 0.050
+
+=cut
+
+use vars qw($VERSION);
+$VERSION = '0.050';
+
+=head1 SYNOPSIS
+
+  use Email::Simple::FileHandle;
+
+  open my $fh, "<", "email.msg";
+
+  my $email = Email::Simple::FromHandle->new($fh);
+
+  print $email->as_string;
+  # or
+  $email->stream_to(\*STDOUT);
+
+=head1 DESCRIPTION
+
+This is a subclass of Email::Simple which can accept filehandles as the source
+of an email.  It will keep a reference to the filehandle and read from it when
+it needs to access the body.  It does not load the entire body into memory and
+keep it there.
+
+=cut
+
+use Carp ();
 use IO::String;
 use Fcntl qw(SEEK_SET);
 
-use vars qw($VERSION);
-$VERSION = '0.011';
+my $crlf = qr/\x0a\x0d|\x0d\x0a|\x0a|\x0d/; # We are liberal in what we accept.
 
-# We are liberal in what we accept.
-# But then, so is a six dollar whore.
-# At least, that's what Casey tells me.
-my $crlf = qr/\x0a\x0d|\x0d\x0a|\x0a|\x0d/;
+=head1 METHODS
+
+In addition to the standard L<Email::Simple> interface, the following methods
+are provided:
+
+=head2 handle
+
+This returns the handle given to construct the message.  If the message was
+constructed with a string instead, it returns an IO::String object.
+
+=cut
 
 sub handle { $_[0]->{handle} }
+
+=head2 body_pos
+
+This method returns the position in the handle at which the body begins.  This
+is used for seeking when re-reading the body.
+
+=cut
+
 sub body_pos { $_[0]->{body_pos} }
+
+=head2 reset_handle
+
+This method seeks the handle to the body position and resets the header-line
+iterator.
+
+For unseekable handles (pipes, sockets), this will die.
+
+=cut
+
+sub reset_handle {
+  my ($self) = @_;
+
+  # Don't die the first time we try to read from a pipe/socket/etc.
+  # TODO: When reading from something non-seekable (body_pos == -1), should we
+  # give the option to store data into a temp file, or something similar?
+  return unless $self->body_pos > 0 or $self->{_seek}++;
+  delete $self->{_get_head_lines};
+
+  seek $self->handle, $self->body_pos, SEEK_SET
+    or Carp::croak "can't seek: $!";
+}
+
+=head2 getline
+
+  $str = $email->getline;
+
+This method returns either the next line from the headers or the next line from
+the underlying filehandle.  It only returns a single line, regardless of
+context.  Returns C<undef> on EOF.
+
+=cut
+
+sub getline {
+  my ($self) = @_;
+  unless ($self->{_get_head_lines}) {
+    $self->{_get_head_lines} = [
+      split(/(?<=\n)/, $self->header_obj->as_string),
+      $self->crlf,
+    ];
+  }
+  my $handle = $self->handle;
+  return shift @{$self->{_get_head_lines}} || <$handle>;
+}
+
+=head2 stream_to
+
+  $email->stream_to($fh, [ \%arg ]);
+
+This method efficiently writes the message to the passed-in filehandle.  
+
+The second argument may be a hashref of options:
+
+=over 4
+
+=item B<reset_handle:>
+
+Whether or not to call C<< $self->reset_handle >> before reading the message
+(default true). 
+
+=item B<chunk_size:>
+
+Number of bytes to read from C<< $self->handle >> at once (default 65536).
+
+=item B<write:>
+
+Coderef to use to print instead of C<print $fh $chunk>.  This coderef will
+receive two arguments, the 'filehandle' (which need not be a real filehandle at
+all) and the current chunk of data.
+
+=back
+
+=cut
+
+sub _stream_to_print {
+  my $fh = shift;
+  print {$fh} @_ or Carp::croak "can't print buffer: $!";
+}
+
+sub stream_to {
+  my ($self, $fh, $arg) = @_;
+  $arg ||= {};
+  $arg->{reset_handle} = 1 unless exists $arg->{reset_handle};
+  # 65536 is a randomly-chosen magical number that's large enough to be a win
+  # over line-by-line reading but small enough not to impinge very much upon
+  # ram usage -- hdp, 2006-11-27
+  $arg->{chunk_size} ||= 65536;
+  $arg->{write}      ||= \&_stream_to_print;
+  $arg->{write}->($fh, $self->header_obj->as_string . $self->crlf);
+  $self->reset_handle if $arg->{reset_handle};
+  my $buf;
+  while (read($self->handle, $buf, $arg->{chunk_size}) > 0) {
+    $arg->{write}->($fh, $buf);
+  }
+}
+
+#### Methods that override Email::Simple below
 
 sub new {
     my ($class, $handle) = @_;
 
     return Email::Simple->new($handle) unless ref $handle;
 
-    my ($head, $mycrlf) = _split_head_from_body($handle);
+    my ($head, $mycrlf) = $class->_split_head_from_body($handle);
 
-    my ($head_hash, $order) = Email::Simple::_read_headers($head);
-
-    bless {
-        head     => $head_hash,
+    my $self = bless {
         handle   => $handle,
         body_pos => tell($handle),
-        order    => $order,
         mycrlf   => $mycrlf,
-        header_names => { map { lc $_ => $_ } keys %$head_hash }
     }, $class;
+
+    $self->header_obj_set(
+        Email::Simple::Header->new($head, { crlf => $self->crlf })
+    );
+
+    return $self;
 }
 
 sub _split_head_from_body {
-    my $handle = shift;
+    my ($class, $handle) = @_;
 
-    my $text = '';
+    my $text = q{};
 
+    # XXX it is stupid to use <> if we're really going to have multiple forms
+    # of crlf, but it is expedient to keep doing so for now. -- hdp, 2006-11-28
+    # theoretically, this should be ok, because it will only fail if lines are
+    # terminated with \x0d, which wouldn't be ok for network transport anyway.
+    my $mycrlf;
     while (<$handle>) {
+        last if $mycrlf and /\A$mycrlf\z/;
         $text .= $_;
-        last if /\A\s*\Z/;
+        ($mycrlf) = /($crlf)\z/;
     }
 
-    my ($head, $crlf) = $text =~ /(.*?($crlf))\2/sm;
-
-    return $crlf ? ($head, $crlf) : ($text, "\n");
-}
-
-sub reset_handle {
-  my ($self) = @_;
-  seek $self->handle, $self->body_pos, SEEK_SET;
+    return ($text, $mycrlf || "\n");
 }
 
 sub body_set {
@@ -69,66 +217,31 @@ sub body_set {
 sub body {
   my $self = shift;
   scalar do {
-    local $/;
+    local $/; ## no critic Local, Punctuation
     $self->reset_handle;
     my $handle = $self->handle;
     <$handle>;
   };
 }
 
-1;
+=head1 PERL EMAIL PROJECT
 
-__END__
+This module is maintained by the Perl Email Project.
 
-=head1 NAME
+L<http://emailproject.perl.org/wiki/Email::Simple::FromHandle>
 
-Email::Simple::FromHandle - an Email::Simple but from a handle
+=head1 AUTHORS
 
-=head1 VERSION
+Ricardo SIGNES wrote Email::Simple.
 
-  $Id: /my/cs/projects/fromhandle/trunk/lib/Email/Simple/FromHandle.pm 22478 2006-06-13T12:51:46.331840Z rjbs  $
+Numerous improvement, especially streamability the handling of pipes, were made
+by Hans Dieter Pearcey.
 
-version 0.011
-
-=head1 SYNOPSIS
-
-  use Email::Simple::FileHandle;
-
-  open my $fh, "<", "email.msg";
-
-  my $email = Email::Simple::FromHandle->new($fh);
-
-  print $email->as_string;
-
-=head1 DESCRIPTION
-
-This is a subclass of Email::Simple which can accept filehandles as the source
-of an email.  It will keep a reference to the filehandle and read from it when
-it needs to access the body.  It does not load the entire body into memory and
-keep it there.
-
-=head1 METHODS
-
-In addition to the standard L<Email::Simple> interface, the following methods
-are provided:
-
-=head2 handle
-
-This returns the handle given to construct the message.  If the message was
-constructed with a string instead, it returns an IO::String object.
-
-=head2 body_pos
-
-This method returns the position in the handle at which the body begins.  This
-is used for seeking when re-reading the body.
-
-=head2 reset_handle
-
-This method seeks the handle to the body position.
-
-=head1 COPYRIGHT
+=head1 COPYRIGHT AND LICENSE
 
 This code is copyright Ricardo SIGNES, 2006.  It is free software, released
 with the same licenses as Perl itself.
 
 =cut
+
+1;
--- t/handle.t
+++ t/handle.t
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 use strict;
-use Test::More tests => 6;
+use Test::More tests => 11;
 use Symbol;
 
 sub read_file   { local $/; local *FH; open FH, shift or die $!; return <FH>; }
@@ -9,14 +9,14 @@ sub file_handle { my $fh = gensym; open $fh, "<", $_[0] or die $!; return $fh }
 use_ok("Email::Simple::FromHandle");
 
 # Very basic functionality test
-my $mail_text   = read_file("t/test-mails/josey-nofold");
-my $mail_handle = file_handle("t/test-mails/josey-nofold");
+my $mail_text   = read_file("t/test-mails/header-blank");
+my $mail_handle = file_handle("t/test-mails/header-blank");
 my $mail = Email::Simple::FromHandle->new($mail_handle);
 
 isa_ok($mail, "Email::Simple");
 isa_ok($mail, "Email::Simple::FromHandle");
 
-like($mail->header('From'), qr/Andrew/, "Andrew's in the header");
+like($mail->header('From'), qr/Business People/, "correct From:");
 
 $mail->reset_handle;
 
@@ -24,12 +24,48 @@ my $handle = $mail->handle;
 
 my @body = <$handle>;
 
-is(@body, 78, "78 lines in body");
+is(@body, 59, "59 lines in body");
 
 $mail->reset_handle;
 
 is(
   <$handle>,
-  "Joanna, All\n",
+  "XyzzyXX,\n",
   "first line gotten as requested",
 );
+
+$mail->reset_handle;
+my $buf;
+$mail->stream_to(
+  \$buf, {
+    write => sub {
+      my ($buf_ref, $chunk) = @_;
+      $$buf_ref .= $chunk;
+    },
+  },
+);
+
+is $buf, $mail->as_string, "stream_to with custom writer";
+
+{
+  pipe(my($rdr, $wtr));
+  unless (fork) {
+    close $rdr;
+    seek $mail_handle, 0, 0;
+    my $mail = Email::Simple::FromHandle->new($mail_handle);
+    $mail->stream_to($wtr);
+    exit 0;
+  }
+  close $wtr;
+  $mail = Email::Simple::FromHandle->new($rdr);
+  my $ref = Email::Simple->new($mail_text);
+  is $mail->body, $ref->body, "body from pipe";
+  is $mail->header('Message-ID'), $ref->header('Message-ID'),
+    "message-id from pipe";
+  # just in case the same bug ever pops up in E::S
+  isnt $mail->header('Message-ID'), '',
+    "message-id from pipe isn't empty";
+  eval { $mail->as_string };
+  like $@, qr/can't seek/i, "illegal seek on pipe";
+}
+
